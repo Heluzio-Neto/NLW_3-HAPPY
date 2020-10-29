a6a29fce338cdf2fac691dffe30003698d9ca35e
dexterc137 <62826121+dexterc137@users.noreply.github.com>
2020-10-29 17:06:06 -0400

    Segundo Commit

--- src/pages/Orphanage.tsx
+++ src/pages/Orphanage.tsx
@@ -18,17 +18,20 @@ interface Orphanage {
   opening_hours: string;
   open_on_weekends: string;
   images: Array<{
+    id: number;
     url: string;
   }>;
 }
 
 interface OrphanageParams {
+
   id: string;
 }
  
 export default function Orphanage() {
   const params = useParams<OrphanageParams>();
   const [orphanage, setOrphanage] = useState<Orphanage>();
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
  
     useEffect(() => {
         api.get(`orphanages/${params.id}`).then(response => {
@@ -48,28 +51,25 @@ export default function Orphanage() {
 
       <main>
         <div className="orphanage-details">
-          <img src={orphanage.images[0].url} alt={orphanage.name}/>
+          <img src={orphanage.images[activeImageIndex].url} alt={orphanage.name}/>
 
 
           <div className="images">
-            <button className="active" type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
-            <button type="button">
-              <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
-            </button>
+             {orphanage.images.map((image, index) => {
+               return (
+                <button 
+                key={image.id} 
+                className={activeImageIndex === index ? 'active' : ''}
+                type="button" 
+                onClick={ () => {
+                  setActiveImageIndex(index);
+                }}
+                >
+                
+               <img src={image.url} alt={orphanage.name} />
+             </button>
+             );
+             })}
           </div>
           
           <div className="orphanage-details-content">
@@ -96,7 +96,7 @@ export default function Orphanage() {
               </Map>
 
               <footer>
-                <a href="">Ver rotas no Google Maps</a>
+                <a target="_blanck" rel="noopener noreferrer" href={`https://www.google.com/maps/dir/?api=1&destination=${orphanage.latitude},${orphanage.longitude}`}>Ver rotas no Google Maps</a>
               </footer>
             </div>
 
@@ -128,10 +128,11 @@ export default function Orphanage() {
 
             </div>
 
-            <button type="button" className="contact-button">
+            {/*<button type="button" className="contact-button">
               <FaWhatsapp size={20} color="#FFF" />
               Entrar em contato
-            </button>
+            </button> */}
+
           </div>
         </div>
       </main>
